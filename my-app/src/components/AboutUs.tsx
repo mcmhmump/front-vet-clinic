4c9127b32a8fbb6e7d3be70295253771c9113c3c
mcmhmump <chuprikova.marina@list.ru>
2026-05-08 11:59:41 +0400

    сделала блок о нас, но буду улучшать

--- my-app/src/components/AboutUs.tsx
+++ my-app/src/components/AboutUs.tsx
@@ -1,13 +1,37 @@
 import wave from '../assets/images/wave.png'
+import mission from '../assets/images/Our Mission.png'
+import yes from '../assets/icons/yes.png'
 export const AboutUs = () => {
     return (
         <section className="relative bg-[#F6F2ED] mt-13">
-            <img src={wave} alt="волна" className='absolute top-[-1%] rotate-180 object-contain w-[1550px] '/>
-            <div className="flex flex-col items-start ">
-                <div className="border-[#F8721F] rounded-[50px] border-[2px] mt-100 ml-15">
-                    <p className="text-[24px] text-[#F8721F] text-center p-2 w-[190px] h-[50px]">О нас</p>
+            <img src={wave} alt="волна" className='absolute top-[-0.2%] rotate-180 object-contain w-[1550px] '/>
+            {/* О нас и заголовок */}
+            <div className="flex flex-col items-start ml-10">
+                <div className="border-[#F8721F] rounded-[50px] border-[2px] mt-40 ">
+                    <p className="text-[24px] text-[#F8721F] font-bold text-center p-2 w-[190px] h-[50px]">О нас</p>
                 </div>
+                <h1 className='text-[56px] text-[#02000F] font-sans leading-tight'>Мы заботимся о <br />наших пациентах</h1>
             </div>
+            {/* Основной контент */}
+            <main className='flex justify-between mt-10 ml-8'>
+                {/* Картинка */}
+                <div>
+                    <img src={mission} alt="миссия" className='w-[677px] object-contain rounded-[4%]'/>
+                </div>
+                {/* Блок с текстом */}
+                <div className='flex flex-col items-start gap-3 w-[688px]'>
+                    <h3 className='text-[28px] text-[#02000F] font-sans'>Наша цель</h3>
+                    <p className='text-[16px] text-[#02000F] font-sans'>Мы верим, что питомцы — это не просто животные, а полноправные члены вашей семьи. Наша ветеринарная клиника создана с безграничной любовью к животным и стремлением оказывать медицинскую помощь высочайшего уровня.</p>
+                    <div className='flex flex-inline gap-3'>
+                        <img src={yes} alt="галочка" className='w-[35px] h-[35px]'/>
+                        <p className='text-[16px] text-[#02000F] font-sans'>Индивидуальный подход – Каждое животное уникально. Мы разрабатываем индивидуальные схемы лечения, реабилитации и диетического питания</p>
+                    </div>
+                    <div className='flex flex-inline gap-3'>
+                        <img src={yes} alt="галочка" className='w-[35px] h-[35px]'/>
+                        <p className='text-[16px] text-[#02000F] font-sans'>Безопасность и контроль – Мы обеспечиваем высшие стандарты ветеринарной помощи. Все помещения клиники строго соответствуют медицинским нормам стерильности и находятся под постоянным наблюдением врачей.</p>
+                    </div>
+                </div>
+            </main>
         </section>
     )
     
