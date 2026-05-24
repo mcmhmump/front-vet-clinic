4541e21e573728abb71d92d422181b3f69c53bc8
mcmhmump <chuprikova.marina@list.ru>
2026-05-24 17:26:15 +0400

    создала страницу для ветеринара и добавила роутер

--- my-app/src/pages/DoctorPage.tsx
+++ my-app/src/pages/DoctorPage.tsx
@@ -1,7 +1,70 @@
+import { DoctorHeader } from "../components/DoctorHeader";
 export const DoctorPage = () => {
+  const appointments = [
+    {
+      owner: "Иванов И.И",
+      pet: "Боня",
+      type: "Хомяк",
+      service: "Осмотр",
+      date: "28.04.26",
+      time: "13:00",
+      phone: "+79172456789",
+    },
+    {
+      owner: "Иванов И.И",
+      pet: "Боня",
+      type: "Хомяк",
+      service: "Осмотр",
+      date: "28.04.26",
+      time: "13:00",
+      phone: "+79172456789",
+    },
+    {
+      owner: "Иванов И.И",
+      pet: "Боня",
+      type: "Хомяк",
+      service: "Осмотр",
+      date: "28.04.26",
+      time: "13:00",
+      phone: "+79172456789",
+    },
+  ];
+
   return (
-    <section>
-      <h1>Doctor page works</h1>
+    <section className="min-h-screen bg-[#F6F2ED] px-4 pb-10">
+      <DoctorHeader />
+
+      <main className="mt-8">
+        {/* Заголовок таблицы */}
+        <div className="grid grid-cols-7 items-center bg-[#EDC3A9] rounded-[8px] px-8 py-5 text-[20px] font-bold text-[#02000F]">
+          <p>Пациент</p>
+          <p>Питомец</p>
+          <p>Вид</p>
+          <p>Услуга</p>
+          <p>Дата</p>
+          <p>Время</p>
+          <p>Телефон</p>
+        </div>
+
+        {/* Строки */}
+        <div className="mt-8">
+          {appointments.map((item, index) => (
+            <div key={index}>
+              <div className="grid grid-cols-7 items-center px-8 py-12 text-[18px] text-[#02000F]">
+                <p>{item.owner}</p>
+                <p>{item.pet}</p>
+                <p>{item.type}</p>
+                <p>{item.service}</p>
+                <p>{item.date}</p>
+                <p>{item.time}</p>
+                <p>{item.phone}</p>
+              </div>
+
+              <div className="h-[4px] bg-[#7C7B82] mx-4 rounded-full"></div>
+            </div>
+          ))}
+        </div>
+      </main>
     </section>
   );
 };
